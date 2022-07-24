6a8ddcb0d7436c260608158117a7eb125a6440e8
Dannel Albert <cuebix@gmail.com>
2022-07-24 12:41:06 -0400

    feat: updated nvchad config

--- custom/plugins/init.lua
+++ custom/plugins/init.lua
@@ -1,3 +1,11 @@
 return {
-  'williamboman/nvim-lsp-installer',
+  -- { 'github/copilot.vim' },
+  { 'williamboman/nvim-lsp-installer' },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+      after = "nvim-lspconfig",
+      config = function()
+         require("custom.plugins.null-ls").setup()
+      end,
+  }
 }
