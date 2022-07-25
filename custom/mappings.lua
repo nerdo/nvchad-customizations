65e85b0777ef09aa00d295ae951cf5967e3dfca7
Dannel Albert <cuebix@gmail.com>
2022-07-25 15:14:14 -0400

    fix: remap commands starting with <leader>c so close has no delay

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -1,160 +1,176 @@
 local M = {}
 
 M.disabled = {
-   n = {
-      -- format
-      ["<leader>fm"] = "",
+	n = {
+		-- format
+		["<leader>fm"] = "",
 
-      -- save
-      ["<C-s>"] = "",
+		-- save
+		["<C-s>"] = "",
 
-      -- close buffer
-      ["<leader>x"] = "",
+		-- close buffer
+		["<leader>x"] = "",
 
-      -- next/prev diagnostics
-      ["[d"] = "",
-      ["d]"] = "",
+		-- next/prev diagnostics
+		["[d"] = "",
+		["d]"] = "",
 
-      -- rename
-      ["<leader>ra"] = "",
+		-- rename
+		["<leader>ra"] = "",
 
-      -- workspaces
-      ["<leader>wa"] = "",
-      ["<leader>wr"] = "",
-      ["<leader>wl"] = "",
+		-- workspaces
+		["<leader>wa"] = "",
+		["<leader>wr"] = "",
+		["<leader>wl"] = "",
 
-      -- whickey
-      ["<leader>wK"] = "",
-      ["<leader>wk"] = "",
+		-- whickey
+		["<leader>wK"] = "",
+		["<leader>wk"] = "",
 
-      -- nvimtree
-      ["<C-n>"] = "",
-      ["<leader>e"] = "",
+		-- nvimtree
+		["<C-n>"] = "",
+		["<leader>e"] = "",
 
-      -- telescope
-      ["<leader>fw"] = "",
-   },
+		-- telescope
+		["<leader>fw"] = "",
+		["<leader>cm"] = "",
+		["<leader>gt"] = "",
+
+		-- lsp
+		["<leader>ca"] = "",
+	},
 }
 
 M.basic = {
-   n = {
-      -- save
-      ["<leader>w"] = { "<cmd> w <CR>", "﬚  save file" },
+	n = {
+		-- save
+		["<leader>w"] = { "<cmd> w <CR>", "﬚  save file" },
 
-      -- close buffer + hide terminal buffer
-      ["<leader>x"] = {
-         function()
-            vim.cmd('w')
-            require("core.utils").close_buffer()
-         end,
-         "   save & close buffer",
-      },
+		-- close buffer + hide terminal buffer
+		["<leader>x"] = {
+			function()
+				vim.cmd("w")
+				require("core.utils").close_buffer()
+			end,
+			"   save & close buffer",
+		},
 
-      ["<leader>c"] = {
-         function()
-            require("core.utils").close_buffer()
-         end,
-         "   close buffer",
-      },
+		["<leader>c"] = {
+			function()
+				require("core.utils").close_buffer()
+			end,
+			"   close buffer",
+		},
 
-      -- workspaces
-      ["<leader>Wa"] = {
-         function()
-            vim.lsp.buf.add_workspace_folder()
-         end,
-         "   add workspace folder",
-      },
+		-- workspaces
+		["<leader>Wa"] = {
+			function()
+				vim.lsp.buf.add_workspace_folder()
+			end,
+			"   add workspace folder",
+		},
 
-      ["<leader>Wr"] = {
-         function()
-            vim.lsp.buf.remove_workspace_folder()
-         end,
-         "   remove workspace folder",
-      },
+		["<leader>Wr"] = {
+			function()
+				vim.lsp.buf.remove_workspace_folder()
+			end,
+			"   remove workspace folder",
+		},
 
-      ["<leader>Wl"] = {
-         function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-         end,
-         "   list workspace folders",
-      },
-   },
+		["<leader>Wl"] = {
+			function()
+				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+			end,
+			"   list workspace folders",
+		},
+	},
 }
 
 M.whichkey = {
-   n = {
-      ["<leader>WK"] = {
-         function()
-            vim.cmd "WhichKey"
-         end,
-         "   which-key all keymaps",
-      },
-      ["<leader>Wk"] = {
-         function()
-            local input = vim.fn.input "WhichKey: "
-            vim.cmd("WhichKey " .. input)
-         end,
-         "   which-key query lookup",
-      },
-   },
+	n = {
+		["<leader>WK"] = {
+			function()
+				vim.cmd("WhichKey")
+			end,
+			"   which-key all keymaps",
+		},
+		["<leader>Wk"] = {
+			function()
+				local input = vim.fn.input("WhichKey: ")
+				vim.cmd("WhichKey " .. input)
+			end,
+			"   which-key query lookup",
+		},
+	},
 }
 
-M.lsp = {
-   n = {
-      -- format
-      ["<leader>lf"] = {
-         function()
-            vim.lsp.buf.formatting()
-         end,
-         "   lsp formatting",
-      },
+M.lspconfig = {
+	n = {
+		-- format
+		["<leader>lf"] = {
+			function()
+				vim.lsp.buf.formatting()
+			end,
+			"   lsp formatting",
+		},
 
-      -- info
-      ["<leader>li"] = {
-         function()
-            vim.diagnostic.open_float()
-         end,
-         "   floating diagnostic",
-      },
+		-- info
+		["<leader>li"] = {
+			function()
+				vim.diagnostic.open_float()
+			end,
+			"   floating diagnostic",
+		},
 
-      -- next/prev diagnostics
-      ["<leader>lk"] = {
-         function()
-            vim.diagnostic.goto_prev()
-         end,
-         "   lsp prev diagnostic",
-      },
+		-- next/prev diagnostics
+		["<leader>lk"] = {
+			function()
+				vim.diagnostic.goto_prev()
+			end,
+			"   lsp prev diagnostic",
+		},
 
-      ["<leader>lj"] = {
-         function()
-            vim.diagnostic.goto_next()
-         end,
-         "   lsp next diagnostic",
-      },
+		["<leader>lj"] = {
+			function()
+				vim.diagnostic.goto_next()
+			end,
+			"   lsp next diagnostic",
+		},
 
-      ["<leader>lr"] = {
-         function()
-            require("nvchad.ui.renamer").open()
-         end,
-         "   lsp rename",
-      },
-   },
+		["<leader>lr"] = {
+			function()
+				require("nvchad.ui.renamer").open()
+			end,
+			"   lsp rename",
+		},
+
+		["<leader>la"] = {
+			function()
+				vim.lsp.buf.code_action()
+			end,
+			"   lsp code_action",
+		},
+	},
 }
 
 M.nvimtree = {
-   n = {
-      -- toggle
-      ["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "   toggle nvimtree" },
+	n = {
+		-- toggle
+		["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "   toggle nvimtree" },
 
-      -- focus
-      ["<leader>E"] = { "<cmd> NvimTreeFocus <CR>", "   focus nvimtree" },
-   },
+		-- focus
+		["<leader>E"] = { "<cmd> NvimTreeFocus <CR>", "   focus nvimtree" },
+	},
 }
 
 M.telescope = {
-   n = {
-      ["<leader>ft"] = { "<cmd> Telescope live_grep <CR>", "   find text (live grep)" },
-   }
+	n = {
+		["<leader>ft"] = { "<cmd> Telescope live_grep <CR>", "   find text (live grep)" },
+
+		-- git
+		["<leader>gl"] = { "<cmd> Telescope git_commits <CR>", "   git log (commits)" },
+		["<leader>gs"] = { "<cmd> Telescope git_status <CR>", "  git status" },
+	},
 }
 
 return M
