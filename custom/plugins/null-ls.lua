f4ffe0d4bfbece0d17688c46f8c257b52a505cde
Dannel Albert <cuebix@gmail.com>
2022-07-25 11:21:47 -0400

    feat: prefer local eslint/prettier and conditional running of them
    eslint runs only if an eslint config is present (not in package.json)
    
    prettier runs only if a prettier config is present or there is no eslint or prettier config

--- custom/plugins/null-ls.lua
+++ custom/plugins/null-ls.lua
@@ -1,25 +1,60 @@
 local present, null_ls = pcall(require, "null-ls")
 
 if not present then
-   return
+	return
 end
 
 local b = null_ls.builtins
 
 local sources = {
-   -- webdev stuff
-   b.formatting.prettier,
-   b.formatting.eslint,
+	-- webdev stuff
+	b.formatting.eslint.with({
+		prefer_local = "node_modules/.bin",
+		condition = function(utils)
+			return utils.root_has_file({
+        ".eslintrc",
+        ".eslintrc.js",
+        ".eslintrc.cjs",
+        ".eslintrc.yaml",
+        ".eslintrc.yml",
+        ".eslintrc.json",
+      })
+		end,
+	}),
+	b.formatting.prettier.with({
+		prefer_local = "node_modules/.bin",
+		condition = function(utils)
+      local has_prettier_config = utils.root_has_file({
+        ".prettierrc",
+        ".prettierrc.js",
+        ".prettierrc.cjs",
+        ".prettierrc.yaml",
+        ".prettierrc.yml",
+        ".prettierrc.json",
+        ".prettierrc.json5",
+        ".prettierrc.toml",
+      })
+      local has_eslint_config = utils.root_has_file({
+        ".eslintrc",
+        ".eslintrc.js",
+        ".eslintrc.cjs",
+        ".eslintrc.yaml",
+        ".eslintrc.yml",
+        ".eslintrc.json",
+      })
+      return has_prettier_config or (not has_prettier_config and not has_eslint_config)
+    end,
+	}),
 
-   -- Lua
-   b.formatting.stylua,
+	-- Lua
+	b.formatting.stylua,
 
-   -- Shell
-   b.formatting.shfmt,
-   b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+	-- Shell
+	b.formatting.shfmt,
+	b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
 }
 
-null_ls.setup {
-   debug = true,
-   sources = sources,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+})
