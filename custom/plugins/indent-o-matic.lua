7edd2f0d08779510bde885fe4a794fda7e88fcc2
Dannel Albert <cuebix@gmail.com>
2022-07-24 22:57:17 -0400

    feat: add indent-o-matic indentation detection

---
+++ custom/plugins/indent-o-matic.lua
@@ -0,0 +1,8 @@
+local present, indent_o_matic = pcall(require, "indent-o-matic")
+
+if not present then
+   return
+end
+
+indent_o_matic.setup {
+}
--- custom/plugins/init.lua
+++ custom/plugins/init.lua
@@ -1,16 +1,22 @@
 return {
-   -- autoclose tags in html, jsx etc
-   ["windwp/nvim-ts-autotag"] = {
-      after = "nvim-treesitter",
-      config = function()
-         require "custom.plugins.nvim-ts-autotag"
-      end,
-   },
+	-- autoclose tags in html, jsx etc
+	["windwp/nvim-ts-autotag"] = {
+		after = "nvim-treesitter",
+		config = function()
+			require("custom.plugins.nvim-ts-autotag")
+		end,
+	},
 
-   ["jose-elias-alvarez/null-ls.nvim"] = {
-      after = "nvim-lspconfig",
-      config = function()
-         require "custom.plugins.null-ls"
-      end,
-   },
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null-ls")
+		end,
+	},
+
+	["Darazaki/indent-o-matic"] = {
+		config = function()
+			require("custom.plugins.indent-o-matic")
+		end,
+	},
 }
