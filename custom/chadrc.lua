ddd85f1f06292882d683d1f7019ce31f1617d345
Dannel Albert <cuebix@gmail.com>
2022-08-03 15:00:59 -0400

    feat: change theme to chadracula, add cursor line

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -1,23 +1,28 @@
 local M = {}
 
-require "custom.functions"
+require("custom.functions")
 
-local overrides = require "custom.overrides"
+local overrides = require("custom.overrides")
 
 vim.opt.scrolloff = 999
 
+M.mappings = require("custom.mappings")
+
 M.ui = {
-  theme = "ayu-dark",
+	theme = "chadracula",
+	hl_override = {
+		CursorLine = {
+			bg = "one_bg",
+		},
+	},
 }
 
-M.mappings = require "custom.mappings"
-
 M.plugins = {
-   override = {
-      ["kyazdani42/nvim-tree.lua"] = overrides.nvimtree,
-      ["williamboman/mason.nvim"] = overrides.mason,
-   },
-   user = require "custom.plugins",
+	override = {
+		["kyazdani42/nvim-tree.lua"] = overrides.nvimtree,
+		["williamboman/mason.nvim"] = overrides.mason,
+	},
+	user = require("custom.plugins"),
 }
 
 return M
