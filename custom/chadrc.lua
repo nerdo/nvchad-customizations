1e71ed448cfad3a1aaadf45cda84d176be592370
Dannel Albert <cuebix@gmail.com>
2022-09-14 13:54:38 -0400

    feat: update nvchad, settings, overrides, indent guides

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -2,8 +2,6 @@ local M = {}
 
 require("custom.functions")
 
-local overrides = require("custom.overrides")
-
 M.mappings = require("custom.mappings")
 
 -- execute the following to see all available highlight groups
@@ -18,8 +16,8 @@ M.ui = {
 
 			base_16 = {
 				base00 = "#0B0E14",
-			}
-		}
+			},
+		},
 	},
 	hl_override = {
 		Visual = {
@@ -39,20 +37,20 @@ M.ui = {
 		},
 		IndentBlanklineContextStart = {
 			bg = "#1A6165",
+			underline = true,
 		},
-	}
+		IndentBlanklineContextChar = {
+			fg = "#1A6165",
+		},
+		IndentBlanklineSpaceCharBlankline = {
+			fg = "#1A6165",
+		},
+	},
 }
 
 -- for some reason setting the TreesitterContext in the theme didn't work...
-vim.api.nvim_set_hl(0, 'TreesitterContext', { bg = "#1A6165" })
+vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#1A6165" })
 
-M.plugins = {
-	override = {
-		["kyazdani42/nvim-tree.lua"] = overrides.nvimtree,
-		["williamboman/mason.nvim"] = overrides.mason,
-		["nvim-treesitter/nvim-treesitter"] = overrides.treesitter,
-	},
-	user = require("custom.plugins"),
-}
+M.plugins = require("custom.plugins")
 
 return M
--- custom/plugins/init.lua
+++ custom/plugins/init.lua
@@ -1,4 +1,94 @@
 return {
+	["folke/which-key.nvim"] = {
+		disable = false,
+	},
+
+	["lukas-reineke/indent-blankline.nvim"] = {
+		override_options = function()
+			vim.g.indent_blankline_char = ""
+
+			if vim.g.vscode then
+				return {
+					use_treesitter = true,
+					space_char_blankline = " ",
+					context_highlight_list = {
+						"IndentBlanklineSpaceCharBlankline"
+					},
+					-- char = "",
+					-- context_char = "",
+					-- context_char_blankline = "",
+				}
+			end
+
+			return {
+				use_treesitter = true,
+				space_char_blankline = " ",
+			}
+		end,
+	},
+
+	["kyazdani42/nvim-tree.lua"] = {
+		override_options = {
+			view = {
+				number = true,
+				relativenumber = true,
+			},
+			actions = {
+				open_file = {
+					quit_on_open = true,
+				},
+			},
+		},
+	},
+
+	["williamboman/mason.nvim"] = {
+		override_options = {
+			ensure_installed = {
+				"html-lsp",
+				"css-lsp",
+				"cssmodules-language-server",
+				"emmet-ls",
+				"intelephense",
+				"json-lsp",
+				"lua-language-server",
+				"rust-analyzer",
+				"tailwindcss-language-server",
+				"typescript-language-server",
+				"yaml-language-server",
+				"svelte-language-server",
+			},
+		},
+	},
+
+	["nvim-treesitter/nvim-treesitter"] = {
+		override_options = {
+			auto_install = true,
+			ensure_installed = {
+				"bash",
+				"html",
+				"css",
+				"dockerfile",
+				"gitignore",
+				"javascript",
+				"json",
+				"jsdoc",
+				"lua",
+				"markdown",
+				"php",
+				"rust",
+				"sql",
+				"scss",
+				"svelte",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vue",
+				"yaml",
+			},
+		},
+	},
+
 	-- smart renaming, preserving case, etc.
 	["tpope/vim-abolish"] = {},
 
@@ -51,9 +141,5 @@ return {
 		config = function()
 			require("custom.plugins.vim-sneak")
 		end,
-	},
-
-	["folke/which-key.nvim"] = {
-		disable = false,
 	},
 }
