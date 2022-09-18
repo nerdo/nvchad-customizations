86fe08a5d367facd1af300ce41c1066362b345ca
Dannel Albert <cuebix@gmail.com>
2022-09-18 19:25:45 -0400

    feat: add go lang support

--- custom/plugins/lspconfig.lua
+++ custom/plugins/lspconfig.lua
@@ -2,6 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 local servers = {
+	"gopls",
 	"rust_analyzer",
 	"intelephense",
 	"svelte",
