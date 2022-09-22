cac473f92ff5412911830c55f41bae9d35b3c4e6
Dannel Albert <cuebix@gmail.com>
2022-09-22 07:44:17 -0400

    feat: add support for pint php formatting

--- custom/plugins/null-ls.lua
+++ custom/plugins/null-ls.lua
@@ -12,42 +12,63 @@ local sources = {
 		prefer_local = "node_modules/.bin",
 		condition = function(utils)
 			return utils.root_has_file({
-        ".eslintrc",
-        ".eslintrc.js",
-        ".eslintrc.cjs",
-        ".eslintrc.yaml",
-        ".eslintrc.yml",
-        ".eslintrc.json",
-      })
+				".eslintrc",
+				".eslintrc.js",
+				".eslintrc.cjs",
+				".eslintrc.yaml",
+				".eslintrc.yml",
+				".eslintrc.json",
+			})
 		end,
 	}),
+
 	b.formatting.prettier.with({
 		prefer_local = "node_modules/.bin",
 		condition = function(utils)
-      local has_prettier_config = utils.root_has_file({
-        ".prettierrc",
-        ".prettierrc.js",
-        ".prettierrc.cjs",
-        ".prettierrc.yaml",
-        ".prettierrc.yml",
-        ".prettierrc.json",
-        ".prettierrc.json5",
-        ".prettierrc.toml",
-      })
-      local has_eslint_config = utils.root_has_file({
-        ".eslintrc",
-        ".eslintrc.js",
-        ".eslintrc.cjs",
-        ".eslintrc.yaml",
-        ".eslintrc.yml",
-        ".eslintrc.json",
-      })
-      return has_prettier_config or (not has_prettier_config and not has_eslint_config)
-    end,
+			local has_prettier_config = utils.root_has_file({
+				".prettierrc",
+				".prettierrc.js",
+				".prettierrc.cjs",
+				".prettierrc.yaml",
+				".prettierrc.yml",
+				".prettierrc.json",
+				".prettierrc.json5",
+				".prettierrc.toml",
+			})
+			local has_eslint_config = utils.root_has_file({
+				".eslintrc",
+				".eslintrc.js",
+				".eslintrc.cjs",
+				".eslintrc.yaml",
+				".eslintrc.yml",
+				".eslintrc.json",
+			})
+			return has_prettier_config or (not has_prettier_config and not has_eslint_config)
+		end,
 	}),
 
 	-- Lua
 	b.formatting.stylua,
+
+	-- PHP
+	-- b.formatting.phpcbf.with({
+	-- 	prefer_local = "./vendor/bin",
+	-- 	condition = function(utils)
+	-- 		local has_phpcbf_installed_locally = utils.root_has_file({
+	-- 			"./vendor/bin/phpcbf",
+	-- 		})
+	-- 		return has_phpcbf_installed_locally
+	-- 	end,
+	-- }),
+
+	b.formatting.pint.with({
+		condition = function(utils)
+			local has_pint_installed_locally = utils.root_has_file({
+				"./vendor/bin/pint",
+			})
+			return has_pint_installed_locally
+		end,
+	}),
 }
 
 null_ls.setup({
