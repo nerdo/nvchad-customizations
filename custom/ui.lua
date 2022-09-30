064957851a36e61610f25c5ebece4ef8db2ee3f1
Dannel Albert <cuebix@gmail.com>
2022-09-30 00:34:01 -0400

    feat: change color theme to ayu-dark and customized it thoroughly

--- custom/ui.lua
+++ custom/ui.lua
@@ -2,54 +2,66 @@
 -- :so $VIMRUNTIME/syntax/hitest.vim
 -- or better yet, launch telescope and search for highlights
 return {
-	theme = "chadracula",
+	theme = "ayu-dark",
 	changed_themes = {
-		chadracula = {
+		['ayu-dark'] = {
 			base_30 = {
 				black = "#0B0E14", --  nvim bg
+				green = "#56c300",
 			},
 
 			base_16 = {
 				base00 = "#0B0E14",
+				base05 = "#E8E7E3",
+				base0A = "#25888E",
+				base0D = "#56c3f9",
+				base0F = "#9A52EF",
 			},
 		},
 	},
 	hl_override = {
+		-- highlight group for importing TS types, super useful visual distinction!
+		TSConstructor = {
+			fg = "#25888E",
+		},
+		Comment = {
+			fg = "#62729D",
+		},
 		Visual = {
-			bg = "#3067C5",
+			bg = "#044A4E",
 		},
 		CursorLine = {
-			bg = "#13294E",
+			bg = "#162F5A",
 		},
 		CursorLineNr = {
-			bg = "#06337F",
+			bg = "#0841A1",
 		},
 		TbLineBufOn = {
-			bg = "#13294E",
+			bg = "#162F5A",
 		},
 		TbLineBufOnModified = {
-			bg = "#13294E",
+			bg = "#162F5A",
 		},
 		TbLineBufOnClose = {
-			bg = "#13294E",
+			bg = "#162F5A",
 		},
 		IndentBlanklineContextStart = {
-			bg = "#1A6165",
-			underline = true,
+			bg = "#0841A1",
+			underline = false,
 		},
 		IndentBlanklineContextChar = {
-			fg = "#1A6165",
+			fg = "#0841A1",
 		},
 		IndentBlanklineSpaceCharBlankline = {
-			fg = "#1A6165",
+			fg = "#0841A1",
 		},
 		TelescopeSelection = {
-			bg = "#06337F",
+			bg = "#0841A1",
 		},
 	},
 	hl_add = {
 		TreesitterContext = {
-			bg = "#1A6165",
+			bg = "#0841A1",
 		},
 		LineNrAbove = {
 			fg = "#C46EA2",
