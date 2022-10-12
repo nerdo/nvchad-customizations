4e19b29419d5371574ee6c7842091aaece563077
Dannel Albert <cuebix@gmail.com>
2022-10-12 00:27:05 -0400

    feat: add font sizing keymaps for gui/neovide

---
+++ custom/gui-font-sizing.lua
@@ -0,0 +1,29 @@
+-- https://github.com/neovide/neovide/issues/1301#issuecomment-1119370546
+vim.g.gui_font_default_size = 16
+vim.g.gui_font_size = vim.g.gui_font_default_size
+vim.g.gui_font_face = "Dank Mono"
+
+RefreshGuiFont = function()
+  vim.opt.guifont = string.format("%s:h%s",vim.g.gui_font_face, vim.g.gui_font_size)
+end
+
+ResizeGuiFont = function(delta)
+  vim.g.gui_font_size = vim.g.gui_font_size + delta
+  RefreshGuiFont()
+end
+
+ResetGuiFont = function ()
+  vim.g.gui_font_size = vim.g.gui_font_default_size
+  RefreshGuiFont()
+end
+
+-- Call function on startup to set default value
+ResetGuiFont()
+
+-- Keymaps
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set({'n', 'i'}, "<D-=>", function() ResizeGuiFont(1)  end, opts)
+vim.keymap.set({'n', 'i'}, "<D-->", function() ResizeGuiFont(-1) end, opts)
+vim.keymap.set({'n', 'i'}, "<D-0>", function() ResetGuiFont() end, opts)
