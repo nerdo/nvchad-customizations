7f2174a092bffd6e1da9b6c7a5b0f0f42872e317
Dannel Albert <cuebix@gmail.com>
2022-10-13 09:30:50 -0400

    feat: hide mouse when typing in neovide

--- custom/neovide.lua
+++ custom/neovide.lua
@@ -6,5 +6,6 @@ vim.g.neovide_input_macos_alt_is_meta = true
 vim.g.neovide_remember_window_size = false
 vim.g.neovide_cursor_vfx_mode = "ripple"
 vim.g.neovide_floating_opacity = 0.9
+vim.g.neovide_hide_mouse_when_typing = true
 
 require("custom.gui-font-sizing")
